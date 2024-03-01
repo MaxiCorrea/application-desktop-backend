a5674160fc48b19e8d3737ba9b2fc517b453cf64
MaxiCorrea <jmaxicorrea@gmail.com>
2024-03-01 19:51:17 -0300

    Documentation API with Swagger V3 & OpenAPI

--- src/main/java/com/github/maxicorrea/applicationdesktopbackend/adapters/input/rest/ApplicationDesktopController.java
+++ src/main/java/com/github/maxicorrea/applicationdesktopbackend/adapters/input/rest/ApplicationDesktopController.java
@@ -2,13 +2,14 @@ package com.github.maxicorrea.applicationdesktopbackend.adapters.input.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.github.maxicorrea.applicationdesktopbackend.application.ports.input.DisplayMenuInputPort;
 import com.github.maxicorrea.applicationdesktopbackend.common.Rest;
 import com.github.maxicorrea.applicationdesktopbackend.domain.Menu;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Rest
 @RestController
+@Tag(name = "Application menu")
 public class ApplicationDesktopController {
 
   private final DisplayMenuInputPort displayMenuInputPort;
